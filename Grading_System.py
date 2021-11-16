31a92f490b7e340e4c22d2f35de6ab78c3d37b80
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2021-11-16 23:34:52 +0800

    Step 1 - 1.0 | Excellent

---
+++ Grading_System.py
@@ -0,0 +1,18 @@
+#Program 1: PUP Grading System
+#Section 8 of https://www.pup.edu.ph/studentservices/files/ThePUPStudentHandbook2014.pdf
+#Create a program that will ask for grade percentage. 
+#Display the equivalent Grade/Mark and Description
+#Input grade: 87.6
+#Grade/Mark: 1.75
+#Description: Very Good
+
+#Grade percentage
+def Grade_percentage():
+    Percentage = float(input("Enter your grade percentage here: "))
+    # step 1 - 1.0 | Excellent 
+    if Percentage >= 97 or Percentage <=100:
+        print ("Grade/Mark: 1.0")
+        print ("Description: Excellent")
+    else:
+        print ("Others")
+
