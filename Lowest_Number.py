4589cae7fafa633f2ddb711f1dc4c112af18e2c5
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2021-11-17 17:20:00 +0800

    Completed the if-else statement

--- Lowest_Number.py
+++ Lowest_Number.py
@@ -21,8 +21,11 @@ Numbers()
 
 #Step 2 - If else statement
 def lowest():
-    if First < Second and Third:
-        print (f"{First} is the lowest number.")
-    else:
-        print ("First number is not the lowest.")
+    if First < Second and First < Third:
+        print (f"{First}, the lowest number is the first number.")
+    elif Second < First and Second < Third:
+        print (f"{Second}, the lowest number is the second number.")
+    elif Third < First and Third < Second:
+        print (f"{Third}, the lowest number is the third number.")
+        
 lowest()
