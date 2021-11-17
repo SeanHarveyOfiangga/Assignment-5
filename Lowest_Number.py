3f8e1e08c29b223f20abdc3f8e7dfb14a19ef10b
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2021-11-17 16:33:30 +0800

    Step 1 - Enter three numbers

---
+++ Lowest_Number.py
@@ -0,0 +1,12 @@
+#Find the lowest number
+#Create a program that ask 3 numbers. 
+#Find the lowest number using only if-else statement.
+#Display the lowest number
+
+#Step 1 - Ask for 3 Numbers
+def Numbers():
+    First = float(input("Enter your first number: "))
+    Second = float(input("Enter your second number: "))
+    Third = float(input("Enter your third number: "))
+    print (f"You picked {First}, {Second}, {Third}.")
+Numbers()
