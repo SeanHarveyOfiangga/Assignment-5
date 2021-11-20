f23a9221548ebadc874982d2cfff2a0b62ab1a65
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2021-11-20 16:32:40 +0800

    Input age done

---
+++ Life_Stages.py
@@ -0,0 +1,16 @@
+#Life stages
+#Create a program that ask for an age of a person
+#Display the life stage of the person.
+#Rules:
+#0 - 12 : Kid
+#13 - 17 : Teen
+#18 : Debut
+#19 above : Adult
+
+#Step 1 - input
+
+def Age():
+    Age = int(input("Please enter your age: "))
+    print (Age)
+
+Age()
